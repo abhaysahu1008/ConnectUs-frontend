f78729093d3745a9615270511341124721a82471
abhaysahu1008@gmail.com <abhaysahugit@gmail.com>
2026-03-08 12:55:09 +0530

    Changed the project name to DevZoo.in

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
       <div className="navbar bg-base-300 shadow-sm ">
         <div className="flex-1">
           <Link to={"/"} className="btn btn-ghost text-xl">
-            <p>🗣️ConnectUs</p>
+            <p>DevZoo.in</p>
           </Link>
         </div>
 
