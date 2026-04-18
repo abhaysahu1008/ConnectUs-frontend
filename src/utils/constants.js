b35fa30a7ab90d156927d06403895f186e6e6331
abhaysahu1008@gmail.com <abhaysahugit@gmail.com>
2026-04-18 20:27:00 +0530

    testing for production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,4 @@
-export const BASE_URL =
-  window.location.hostname === "localhost" ||
-  window.location.hostname === "127.0.0.1"
+const BASE_URL =
+  window.location.hostname === "localhost"
     ? "http://localhost:7777"
-    : "/api";
+    : "https://devzoo.in";
